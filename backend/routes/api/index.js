8989b5da108505a9d862c09eafbd624c5f17d8eb
Jacob <EarthTek19@gmail.com>
2025-06-03 23:57:01 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,4 +4,13 @@ router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
 module.exports = router;
